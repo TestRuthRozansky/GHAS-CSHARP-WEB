35a02f405cf45a3655c6905b9daab2ea3fff3443
e0067804 <ruthrozansky@eaton.com>
2022-07-31 15:16:39 -0400

    first commit

---
+++ GHAS-CSHARP-WEB/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
+
+namespace GHAS_CSHARP_WEB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var pwd = Request.Form["password"];
+            Regex testPassword = new Regex(pwd);
+            var b = Int16.Parse(pwd);
+            ViewData["TestDiv"] =  testdiv(1, b);
+        }
+
+        int testdiv(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
